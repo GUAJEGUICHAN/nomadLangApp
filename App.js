36304f0ff588ba24d9f32a06c004c6450d417032
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-02-12 18:27:38 +0900

    #4.8 ValueXY (11:26)

--- App.js
+++ App.js
@@ -1,21 +1,99 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useRef } from 'react';
+
+import { Animated, TouchableOpacity, Easing, Pressable, Dimensions } from 'react-native';
+
+import styled from 'styled-components/native';
+
+const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get('window')
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+const Box = styled.View`
+  background-color: tomato;
+  width: 200px;
+  height: 200px;
+`;
+
+const AnimatedBox = Animated.createAnimatedComponent(Box)
 
 export default function App() {
+  const [up, setUp] = useState(false);
+  const POSITION = useRef(new Animated.ValueXY({ x: -SCREEN_WIDTH / 2 + 100, y: -SCREEN_HEIGHT / 2 + 100 })).current;
+  const toggleUp = () => setUp((prev) => !prev)
+
+  const topLeft = Animated.timing(POSITION, {
+    toValue: {
+      x: -SCREEN_WIDTH / 2 + 100,
+      y: -SCREEN_HEIGHT / 2 + 100,
+    },
+    useNativeDriver: false,
+  });
+  const bottomLeft = Animated.timing(POSITION, {
+    toValue: {
+      x: -SCREEN_WIDTH / 2 + 100,
+      y: +SCREEN_HEIGHT / 2 - 100,
+    },
+    useNativeDriver: false,
+  });
+  const bottomRight = Animated.timing(POSITION, {
+    toValue: {
+      x: +SCREEN_WIDTH / 2 - 100,
+      y: +SCREEN_HEIGHT / 2 - 100,
+    },
+    useNativeDriver: false,
+  });
+  const topRight = Animated.timing(POSITION, {
+    toValue: {
+      x: +SCREEN_WIDTH / 2 - 100,
+      y: -SCREEN_HEIGHT / 2 + 100,
+    },
+    useNativeDriver: false,
+  });
+
+  const moveUp = () => {
+    Animated.loop(Animated.sequence([
+      bottomLeft,
+      bottomRight,
+      topRight,
+      topLeft
+    ])).start()
+  }
+
+  const borderRadius = POSITION.y.interpolate({
+    inputRange: [-200, 200],
+    outputRange: [100, 0]
+  })
+
+  // const rotation = POSITION.y.interpolate({
+  //   inputRange: [-200, 200],
+  //   outputRange: ["-360deg", "360deg"]
+  // })
+
+  const bgColor = POSITION.y.interpolate({
+    inputRange: [-200, 200],
+    outputRange: ["rgb(255,99,71)", "rgb(71,166,255)"],
+  })
+
   return (
-    <View style={styles.container}>
-      <Text>Test</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <Pressable
+        onPress={moveUp}
+      >
+        <AnimatedBox
+          style={{
+            backgroundColor: bgColor,
+            transform: [
+              // { rotateY: rotation },
+              ...POSITION.getTranslateTransform()
+            ],
+            borderRadius: borderRadius
+          }}
+        />
+      </Pressable>
+    </Container>
+
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
